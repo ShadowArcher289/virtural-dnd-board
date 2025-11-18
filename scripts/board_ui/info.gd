a8f1b43497cb72c5ae80ca1e8b21978858991064
ShadowArcher289 <ahasuncion21@gmail.com>
2025-11-18 16:59:49 -0500

    re-worked the info tab into the menu with the add and create tabs

---
+++ scripts/board_ui/info.gd
@@ -0,0 +1,5 @@
+extends MarginContainer
+
+@onready var creature_name: RichTextLabel = $VBoxContainer/CreatureName
+@onready var creature_stats: RichTextLabel = $VBoxContainer/CreatureStats
+@onready var creature_description: RichTextLabel = $VBoxContainer/CreatureDescription
