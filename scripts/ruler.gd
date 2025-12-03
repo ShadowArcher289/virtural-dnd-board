6eacb47892dbdc35a24d00a9ad909af56c24e4de
ShadowArcher289 <ahasuncion21@gmail.com>
2025-12-02 23:13:19 -0500

    Modified ruler.gd so that it could be used for AOE attacks

--- scripts/ruler.gd
+++ scripts/ruler.gd
@@ -2,18 +2,26 @@ extends Node3D
 
 @onready var point_1: MeshInstance3D = $Point1
 @onready var point_2: MeshInstance3D = $Point2
-@onready var line: MeshInstance3D = $Line
-@onready var distance: Label3D = $Line/Distance
+@onready var main_line: MeshInstance3D = $MainLine
+@onready var distance: Label3D = $MainLine/Distance
 
-enum State {
+enum PlaceState { ## The state for placing a point
 	PLACE_POINT_1,
 	PLACE_POINT_2,
 	IDLE
 }
 
+enum State {
+	RULER,
+	ATK_AOE_CONE,
+	ATK_AOE_CIRCLE,
+	ATK_AOE_SQUARE
+}
+
 const RULER_DISTANCE_MULTIPLIER = 6; ## Number to multiply the raw distance to get the D&D 5m per square distance
 
-var current_state = State.IDLE;
+var current_place_state = PlaceState.IDLE;
+var current_state = State.RULER;
 
 var switch_count = 0;
 
@@ -39,18 +47,21 @@ func _process(_delta: float) -> void:
 	
 	if MouseCollision.mouse_raycast_data != null && MouseCollision.mouse_raycast_data.get("position") != null && MouseCollision.currentState("measure"):
 		
-		match current_state:
-			State.PLACE_POINT_1:
+		match current_place_state:
+			PlaceState.PLACE_POINT_1:
 				point_1.show();
 				point_1.global_position = MouseCollision.mouse_raycast_data.get("position");
-			State.PLACE_POINT_2:
+			PlaceState.PLACE_POINT_2:
 				point_2.show();
 				point_2.global_position = MouseCollision.mouse_raycast_data.get("position");
-	elif(!MouseCollision.currentState("measure")):
+	elif(!MouseCollision.currentState("measure") || !MouseCollision.currentState("attack_area")):
 		point_1.hide();
 		point_2.hide();
 	
-	place_line();
+	match current_state:
+		State.RULER:
+			place_line(point_1, point_2, main_line);
+			
 
 #func place_point() -> void: ## place points 1 and 2 on the map.
 	#match switch_count:
@@ -77,27 +88,27 @@ func _process(_delta: float) -> void:
 func switch_state(state: String) -> void: ## Switch the ruler's state
 	match state.to_lower():
 		"place_point_1":
-			current_state = State.PLACE_POINT_1;
+			current_place_state = PlaceState.PLACE_POINT_1;
 		"place_point_2":
-			current_state = State.PLACE_POINT_2;
+			current_place_state = PlaceState.PLACE_POINT_2;
 		"idle":
-			current_state = State.IDLE;
+			current_place_state = PlaceState.IDLE;
 		_:
 			print_debug("Invalid State: " + state);
 
-func place_line() -> void:
-	if(point_1.is_visible_in_tree() && point_2.is_visible_in_tree()): # show the line only if both points are visible
-		line.show();
+func place_line(p1: MeshInstance3D, p2: MeshInstance3D, given_line: MeshInstance3D) -> void: ## place a given line between two given points
+	if(p1.is_visible_in_tree() && p2.is_visible_in_tree()): # show the line only if both points are visible
+		given_line.show();
 		
 		# set the line to be in-between the two points
-		line.global_position = ((point_2.global_position - point_1.global_position)/2) + point_1.global_position;
-		line.mesh.height = sqrt(pow(abs(point_2.global_position.x - point_1.global_position.x), 2) + pow(abs(point_2.global_position.y - point_1.global_position.y), 2) + pow(abs(point_2.global_position.z - point_1.global_position.z), 2));
-		if(point_1.global_position != point_2.global_position):
-			line.look_at(point_2.global_position); # line's rotation
-			line.rotation_degrees.x += 90;
+		given_line.global_position = ((p2.global_position - p1.global_position)/2) + p1.global_position;
+		given_line.mesh.height = sqrt(pow(abs(p2.global_position.x - p1.global_position.x), 2) + pow(abs(p2.global_position.y - p1.global_position.y), 2) + pow(abs(p2.global_position.z - p1.global_position.z), 2));
+		if(p1.global_position != p2.global_position):
+			given_line.look_at(p2.global_position); # line's rotation
+			given_line.rotation_degrees.x += 90;
 		else:
-			line.hide();
+			given_line.hide();
 		
-		distance.text = str((round(line.mesh.height * 100)/100) * RULER_DISTANCE_MULTIPLIER) + "m";
+		distance.text = str((round(main_line.mesh.height * 100)/100) * RULER_DISTANCE_MULTIPLIER) + "m";
 	else: # hide the line otherwise
-		line.hide();
+		given_line.hide();
