a58df16326c5c0e7023e6f0ed2677d9b721f7d05
ShadowArcher289 <ahasuncion21@gmail.com>
2025-12-11 22:01:32 -0500

    Edit to board_menu so the game will always run with the first tab open in the menu regardless of what's in the editor

--- scripts/board_ui/board_menu.gd
+++ scripts/board_ui/board_menu.gd
@@ -4,6 +4,7 @@ extends FoldableContainer
 
 func _ready() -> void:
 	SignalBus.creature_selected.connect(_creature_selected);
+	tab_container.current_tab = 0; # start the current tab at 0 to keep consistency even if editor is not setup right.
 
 func _creature_selected(_creature: Dictionary) -> void: ## Switch tab to the info tab when a creature is selected
 	if (MouseCollision.currentState("info")):
